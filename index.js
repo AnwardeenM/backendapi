8e1bc102a6354c84c730d2ef638a0d90b9018a75
AnwardeenM <kalilanwar597@gmail.com>
2022-12-17 21:32:51 +0530

    Created:The Backend Fetching data from the api and Stored in MongoDB

---
+++ index.js
@@ -0,0 +1,55 @@
+import fetch from "node-fetch";
+
+import mongoose from "mongoose";
+
+mongoose.set('strictQuery', true);
+
+mongoose.connect("mongodb://127.0.0.1:27017/UMS");
+
+const userSchema = new mongoose.Schema({
+    email:{
+        type:String,
+        required:true
+    },
+    name:{
+        type:Object,
+        required:true
+    },
+    gender:{
+        type:String,
+        required:false
+    },
+    location:{
+        type:Object,
+        required:true
+    },
+   picture:{
+        type:Object,
+        required:false
+    }
+})
+
+const Users = mongoose.model("Post",userSchema);
+
+async function getUsers(){
+    
+    const users = await fetch("https://randomuser.me/api/?results=50")
+    const response = await users.json();
+    const sized = response.results;
+    // console.log(sized);
+
+    for(let i=0;i<sized.length;i++){
+        console.log(sized[i]["name"]);
+
+       const user = new Users({
+            email:sized[i]["email"],
+            name:sized[i]["name"],
+             gender:sized[i]["gender"],
+            location:sized[i]["location"],
+            picture:sized[i]["picture"]
+        })
+       user.save();        
+    }
+}
+
+getUsers();
